6b37c6f4c3e8ce1bd41075e47ea149a0131088e9
Aleks Golovach <aleks.golv@gmail.com>
2019-05-08 22:21:49 +0300

    adding functions to calculate

--- Calculate.java
+++ Calculate.java
@@ -3,6 +3,22 @@ public class Calculate {
 public static void main(String[] args) {
 
 System.out.println("Calculate...");
+	int first = Integer.valueOf(args[0]);
+	int second = Integer.valueOf(args[1]);
+	int sum = first + second;
+	int subst = first - second;
+	int divi = first / second;
+	int multi = first * second;
+	
+	
+	
+	System.out.println("Sum " + sum);
+	System.out.println("Substraction " + subst);
+	System.out.println("Division " + divi);
+	System.out.println("Multiplication " + multi);
+	
+	
+
 }
 
 }
